8095994a0bfde547f2eea57fd3833437c811b96a
Maria Kaninia <maria.ev.kaninia@gmail.com>
2024-01-18 12:34:44 +0100

    Update main.py
    Plotly line plot for COPX

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
+import plotly.express as px
+
 st.title('Bollinger Bands')
+
+df = pd.read_csv('copx.csv')
+fig = px.line(df, x='Date', y='Adj Close')
+fig.update_layout(
+    title="COPX",
+)
+
+st.write(fig)
+
+
