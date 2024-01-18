10e214cbc7e082f399b8bd49dab2caf3faa7271d
Maria Kaninia <maria.ev.kaninia@gmail.com>
2024-01-18 17:01:59 +0100

    Update main.py
    Bollinger bands, fixed 20d

--- main.py
+++ main.py
@@ -6,14 +6,30 @@ import pandas as pd
 
 st.title('Bollinger Bands')
 
-st.write('COPX')
-
 df = pd.read_csv('copx.csv')
-fig = px.line(df, x='Date', y='Adj Close')
-fig.update_layout(
-    title="COPX",
-)
+
+fig = go.Figure()
+
+# Add the price chart
+fig.add_trace(go.Scatter(x=df['Date'], y=df['Adj Close'], mode='lines', name='Price', line={"color": "black"}))
+
+# Add the Upper Bollinger Band (UB) and shade the area
+fig.add_trace(go.Scatter(x=df['Date'], y=df['UB'], mode='lines'
+                         , name='Upper Bollinger Band'
+                         , line={"color": "red"}))
+fig.add_trace(go.Scatter(x=df['Date'], y=df['LB'], fill='tonexty', mode='lines'
+                         , name='Lower Bollinger Band'
+                         , line={"color": "rgb(0, 0, 255, 0.01)"}
+                         ))
+
+# Add the Middle Bollinger Band (MA)
+fig.add_trace(go.Scatter(x=df['Date'], y=df['SMA'], mode='lines', name='Middle Bollinger Band', line={"color": "blue"}))
+
+# Customize the chart layout
+fig.update_layout(title='COPX Stock Price with Bollinger Bands',
+                  xaxis_title='Date',
+                  yaxis_title='Price',
+                  showlegend=True)
 
 st.write(fig)
-
 
